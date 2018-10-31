86adb7b7d8218f9db6c08118af64f6193c4c0dea
poohRui <yuruil@qq.com>
2018-10-31 11:46:31 +0800

    Add comments to cuda_gemm

--- cublas_gemm/gemm.cu
+++ cublas_gemm/gemm.cu
@@ -3,7 +3,16 @@
 #include <cuda.h>
 #include "cublas_v2.h"
 
-
+/**
+ * This is a stub function which call gemm to calculate "matrix * matrix" problem
+ *
+ *@param A    an input matrix with size m*dim
+ *@param B    an input matrix with size dim*n
+ *@param C    an output matrix with size m*n
+ *@param m    size of the first dimension in A
+ *@param n    size of the second dimension in B
+ *@param dim  size of the second dimension in A
+ */
 void gemm_cuda(float* A,
                float* B,
                float* C,
@@ -21,6 +30,7 @@ void gemm_cuda(float* A,
     size_t size_pitch_bytes_C;
     
     // A(m*dim), B(dim*n), C(m*n)
+    // Allocate space and transfer data to device
     cudaMallocPitch((void**)&d_A, &size_pitch_bytes_A, dim * size_of_float, m);
     cudaMallocPitch((void**)&d_B, &size_pitch_bytes_B, n * size_of_float, dim);
     cudaMallocPitch((void**)&d_C, &size_pitch_bytes_C, n * size_of_float, m);
@@ -28,14 +38,16 @@ void gemm_cuda(float* A,
     cudaMemcpy2D(d_A, size_pitch_bytes_A, A, dim * size_of_float, dim * size_of_float, m, cudaMemcpyHostToDevice);
     cudaMemcpy2D(d_B, size_pitch_bytes_B, B, n * size_of_float, n * size_of_float, dim, cudaMemcpyHostToDevice);
     
-    // 调用cublas的gemm的kernel，做矩阵乘矩阵的计算
+    // Call gemm function in cublas
     cublasHandle_t cublasHandle;
     cublasCreate(&cublasHandle);
     
+    // Pay attention to the parameters
     float alpha = 1.0;
     float beta = 0.0;
     cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, dim, &alpha, d_B, n, d_A, dim, &beta, d_C, n);
     
+    // Transfer result back
     cudaMemcpy2D(C, n * size_of_float, d_C, size_pitch_bytes_C, n * size_of_float, m, cudaMemcpyDeviceToHost);
     
     cudaFree(d_A);
